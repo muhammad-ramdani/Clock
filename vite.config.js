25890932a90d3eff5f985916af6587da0358358f
Muhammad Ramdani <muhammadramdanibjrs@gmail.com>
2024-12-09 18:10:20 +0700

    Menambahkan konfigurasi GitHub Actions untuk deploy

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: 'https://muhammad-ramdani.github.io/clock/',
+    build: {
+        outDir: 'dist' // Direktori hasil build
+    }
+});
