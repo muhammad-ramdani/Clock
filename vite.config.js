f23499e681da698f07c92e9e380a4647060bf631
Muhammad Ramdani <muhammadramdanibjrs@gmail.com>
2024-12-09 22:47:14 +0700

    add headers configuration in deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     base: '/clock/',
     build: {
-        outDir: 'dist' // Direktori hasil build
+        outDir: 'dist'
     }
 });
