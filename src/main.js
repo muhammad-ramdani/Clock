f329fc6261822b48f3abe930d1d6d4e9c8e30fe8
Muhammad Ramdani <muhammadramdanibjrs@gmail.com>
2024-12-09 17:03:46 +0700

    Migrasi ke Vite

---
+++ src/main.js
@@ -0,0 +1,37 @@
+// Function to update the clock
+const updateClock = () => {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+
+    // Update hours and minutes
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+
+    // Toggle colon visibility for blinking effect
+    const colon = document.getElementById("colon");
+    colon.style.visibility = now.getSeconds() % 2 === 0 ? "visible" : "hidden";
+};
+
+updateClock();
+setInterval(updateClock, 1000);
+
+
+// Toggle fullscreen mode on double click
+document.body.addEventListener("dblclick", () => {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+});
+
+
+// Set cursor to none after 2 seconds
+let timeout = setTimeout(() => (document.body.style.cursor = "none"), 2000);
+
+document.body.addEventListener("mousemove", () => {
+    document.body.style.cursor = "default";
+    clearTimeout(timeout);
+    timeout = setTimeout(() => (document.body.style.cursor = "none"), 2000);
+});
